42dfe96ba798e59b3ef47d6e243c37f1b3aec84c
Kang Sheng Tay <kangsheng@gmail.com>
2017-03-13 18:57:00 +0800

    Registration is done

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,114 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+# before_action :configure_sign_up_params, only: [:create]
+# before_action :configure_account_update_params, only: [:update]
+
+  # GET /resource/sign_up
+  def new
+    build_resource({})
+    yield resource if block_given?
+    respond_with resource
+  end
+
+  # POST /resource
+  def create
+    build_resource(sign_up_params)
+
+    resource.save
+    yield resource if block_given?
+    if resource.persisted?
+      if resource.active_for_authentication?
+        set_flash_message! :notice, :signed_up
+        sign_up(resource_name, resource)
+        respond_with resource, location: after_sign_up_path_for(resource)
+      else
+        set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+        expire_data_after_sign_in!
+        respond_with resource, location: after_inactive_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      # respond_with resource, location: new_user_registration_path
+      # set_flash_message! :alert, resource.errors.full_messages
+
+      if resource.errors['email'][0]
+        flash['email'] = { "type"=>"error", "message"=>resource.errors['email'], "value"=>resource['email'] }
+      else
+        flash['email'] = { "value"=>resource['email'] }
+      end
+
+      if resource.errors["name"][0]
+        flash['name'] = { "type"=>"error", "message"=>resource.errors['email'], "value"=>resource['name'] }
+      else
+        flash['name'] = { "value"=>resource['name'] }
+      end
+
+      if resource.errors["password"]
+        flash['password'] = { "type"=>"error", "message"=>resource.errors["password"] }
+      end
+
+      if resource.errors["password_confirmation"]
+        flash['password_confirmation'] = { "type"=>"error", "message"=>resource.errors['password_confirmation']}
+      end
+      
+      redirect_to new_user_registration_path
+    end
+  end
+
+  # GET /resource/edit
+  # def edit
+  #   super
+  # end
+
+  # PUT /resource
+  # def update
+  #   super
+  # end
+
+  # DELETE /resource
+  # def destroy
+  #   super
+  # end
+
+  # GET /resource/cancel
+  # Forces the session data which is usually expired after sign
+  # in to be expired now. This is useful if the user wants to
+  # cancel oauth signing in/up in the middle of the process,
+  # removing all OAuth session data.
+  # def cancel
+  #   super
+  # end
+
+  protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_up_params
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
+  # end
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_account_update_params
+  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
+  # end
+
+  # The path used after sign up.
+  # def after_sign_up_path_for(resource)
+  #   super(resource)
+  # end
+
+  # The path used after sign up for inactive accounts.
+  def after_inactive_sign_up_path_for(resource)
+    # super(resource)
+    '/users/sign_up'
+  end
+
+  private
+
+  def sign_up_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def account_update_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password)
+  end
+end
