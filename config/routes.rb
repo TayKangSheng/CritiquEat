62c2dcef7c277abf812053b461e2b13e0ba1f3a3
Kang Sheng Tay <kangsheng@gmail.com>
2017-03-14 11:16:30 +0800

    updated devise routes from /users to /profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  devise_for :users, :controllers => { registrations: 'users/registrations' }
+  devise_for :users, :path => "profile", :controllers => { registrations: 'users/registrations' }
+
+  resource :users, :path => "profile", only: [:show]
   root 'static#index'
 
 end
