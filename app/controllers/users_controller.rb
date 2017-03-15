74cc3a4e5c89c28ae6531080ca0d85e9dc028685
Kang Sheng Tay <kangsheng@gmail.com>
2017-03-15 16:44:40 +0800

    user profile page done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,14 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!
+  before_action :set_user
+  # before_action :authenticate_user!
+
   def show
   end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
 end
