81c1511abc8808fb575e357aed243e284ae4642a
Kang Sheng Tay <kangsheng@gmail.com>
2017-03-17 11:13:08 +0800

    limit tops to 3 only

--- app/models/user_save_review.rb
+++ app/models/user_save_review.rb
@@ -6,10 +6,12 @@ class UserSaveReview < ApplicationRecord
     select("reviews.user_id, count(reviews.user_id)")
     .joins("INNER JOIN reviews on user_save_reviews.review_id = reviews.id")
     .group("reviews.user_id")
+    .limit(3)
   end
 
   def self.topbookmarkedcritiqueat
     select("review_id, count(review_id)")
     .group("user_save_reviews.review_id")
+    .limit(3)
   end
 end
