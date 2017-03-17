82ed9d7c16d6db908b13ddce3b420ea470fda47e
Kang Sheng Tay <kangsheng@gmail.com>
2017-03-17 11:17:09 +0800

    ordering

--- app/models/user_save_review.rb
+++ app/models/user_save_review.rb
@@ -6,12 +6,14 @@ class UserSaveReview < ApplicationRecord
     select("reviews.user_id, count(reviews.user_id)")
     .joins("INNER JOIN reviews on user_save_reviews.review_id = reviews.id")
     .group("reviews.user_id")
+    .order("count(reviews.user_id) DESC")
     .limit(3)
   end
 
   def self.topbookmarkedcritiqueat
     select("review_id, count(review_id)")
     .group("user_save_reviews.review_id")
+    .order("count(review_id) DESC")
     .limit(3)
   end
 end
